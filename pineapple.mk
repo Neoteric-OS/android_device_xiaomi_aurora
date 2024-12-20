821311a748405b6db9fd086f0d9298d82aca5c12
Akash Srivastava <akashniki@gmail.com>
2024-12-20 15:43:52 +0100

    aurora: Switch to OSS AIDL thermal HAL

--- pineapple.mk
+++ pineapple.mk
@@ -580,6 +580,10 @@ PRODUCT_PACKAGES += \
 BOARD_SHIPPING_API_LEVEL := 34
 PRODUCT_SHIPPING_API_LEVEL := 34
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.qti
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
