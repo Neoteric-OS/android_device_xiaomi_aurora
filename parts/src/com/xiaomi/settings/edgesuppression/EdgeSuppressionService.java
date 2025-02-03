1b24451a823aa41c98902401450709b3770713fa
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-02-03 15:31:43 +0100

    aurora: parts: Make sure to reset edge rejection on every panel on
     * apparently the configuration sometimes gets lost on panel reset
    
    Change-Id: I2be29370049b871cc0b68af1d2d0a66820914094

--- parts/src/com/xiaomi/settings/edgesuppression/EdgeSuppressionService.java
+++ parts/src/com/xiaomi/settings/edgesuppression/EdgeSuppressionService.java
@@ -7,7 +7,10 @@
 package com.xiaomi.settings.edgesuppression;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.IntentFilter;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
@@ -22,6 +25,15 @@ public class EdgeSuppressionService extends Service {
 
     private EdgeSuppressionManager mEdgeSuppressionManager;
 
+    private final BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (DEBUG) Log.d(TAG, "onReceive: " + intent.getAction());
+            if (intent.getAction() == Intent.ACTION_SCREEN_ON)
+                mEdgeSuppressionManager.handleEdgeSuppressionChange();
+        }
+    };
+
     @Override
     public void onCreate() {
         if (Build.SKU.equals("aurora")) {
@@ -30,6 +42,8 @@ public class EdgeSuppressionService extends Service {
 
             // Initialize EdgeSuppressionManager
             try {
+                IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+                registerReceiver(mScreenStateReceiver, screenStateFilter);
                 mEdgeSuppressionManager = EdgeSuppressionManager.getInstance(getApplicationContext());
                 if (mEdgeSuppressionManager == null) {
                     Log.e(TAG, "Failed to initialize EdgeSuppressionManager");
@@ -80,6 +94,7 @@ public class EdgeSuppressionService extends Service {
     @Override
     public void onDestroy() {
         if (DEBUG) Log.d(TAG, "onDestroy");
+        unregisterReceiver(mScreenStateReceiver);
         super.onDestroy();
     }
 
